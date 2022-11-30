9e2e0344d541ca525874e52d3df814578c5bb29b
Cushz <arif.abdullayev@ufaz.az>
2022-11-30 11:02:42 +0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import Auth from "./Components/auth";
+import Cards from "./Components/Cards.js"
 import React from 'react';
 import {BrowserRouter as Router, Route, Routes, Link} from "react-router-dom";
 function App() {
   return (
-   <Auth />
+    <Router>
+           <div className="App">
+           <Routes>
+                 <Route exact path='/Auth' element={<Auth />}></Route>
+                 <Route exact path='/About' element={<Cards />}></Route>
+          </Routes>
+          </div>
+       </Router>
   )
   
 }
