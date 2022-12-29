6515fb69b5fb435ab0f9ae6b5f3f95be42bebfbc
Kamal <elesgerlikamal04@gmail.com>
2022-12-29 23:59:44 +0400

    connected to backend

--- src/Components/ProfileUpdate/ProfileUpdate.js
+++ src/Components/ProfileUpdate/ProfileUpdate.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {useState} from "react"
 import "./ProfileUpdate.css"
-import { Box,SimpleGrid,Avatar, FormLabel,Input, Flex, Text, Button, Heading,FormControl} from "@chakra-ui/react";
+import { Box,SimpleGrid,Avatar, FormLabel,Input, Flex, Text, Button, Heading,FormControl, Modal, useDisclosure, ModalOverlay, ModalContent, ModalHeader, ModalBody, ModalFooter, ModalCloseButton} from "@chakra-ui/react";
+import { getUserInfo } from '../../api/getUserInfo';
+import { updateUserInfo } from '../../api/updateUserInfo';
+
 export default function ProfileUpdate() {
     const database = ["https://bit.ly/dan-abramov","https://bit.ly/prosper-baba","https://bit.ly/kent-c-dodds","https://bit.ly/ryan-florence"];
     const [profile, setProfile] = useState("https://bit.ly/dan-abramov")
     const [index, setIndex] = useState(0)
+    const [errorMessage, setErrorMessage] = useState(null);
+
+    const [originalUser, setOriginalUser] = useState({firstname: '', lastname: '', date_of_birth: ''});
+    const [user, setUser] = useState({firstname: '', lastname: '', date_of_birth: ''});
+
+    const {isOpen, onOpen, onClose} = useDisclosure();
+
+    useEffect(() => {
+      async function getUser() {
+        const data = await getUserInfo();
+        if (!data) {
+            setErrorMessage("Something went wrong");
+            return; 
+        }
+        setUser(data);
+        setOriginalUser(data);
+      }
+      getUser(); 
+    }, []);
+
     function onPhotoChange () 
     {  
        setProfile(database[index])
@@ -15,14 +38,36 @@ export default function ProfileUpdate() {
         setIndex(0)
        }
        
-       console.log(profile)
     }
+
+    const onSave = async () => {
+
+      const updateUserDto = {
+        firstname: user.firstname,
+        lastname: user.lastname,
+        date_of_birth: user.date_of_birth,
+      }
+      
+      const data = await updateUserInfo(updateUserDto);
+      if (!data) {
+          setErrorMessage("Something went wrong");
+          return; 
+      }
+      window.location.reload(false);
+    }
+
+    if (errorMessage) {
+        return <div>{errorMessage}</div>;
+    }
+
+    
+
     return (
     
    <Flex className='main-profile-wrapper' >
     <Box  className="box-header" h={"30%"} >
         <Box>
-        <Heading>Profile</Heading>
+        <Heading>Hello {originalUser.firstname}!</Heading>
     <Text>Update your photos and personal details here.</Text>
         </Box>
     <Box justifySelf={"flex-end"}>
@@ -35,37 +80,64 @@ export default function ProfileUpdate() {
     </Box>
 
     <Box className="box-body" h={"60%"} >
-    <form method="POST" action="/Users/nicat/Desktop/themindfront/The_Mind_Front_End/public">
+    
       <FormControl>
         <SimpleGrid columns={2}>
-            <Box class="Binput">
-            <FormLabel fontWeight={"bolder"} for="Name">Name</FormLabel>
-        <Input placeholder='Name' name="Name" bg={"white"} />
-            </Box>
-        
-        <Box class="Binput">
-        <FormLabel fontWeight={"bolder"} for="Surname" >Surname</FormLabel>
-        <Input placeholder='Surname'  bg={"white"} name="Surname" />
+
+        <Box className="Binput">
+        <FormLabel fontWeight={"bolder"} htmlFor="Name">Name</FormLabel>
+        <Input placeholder='Name' id="Name" bg={"white"} value={user.firstname} onChange={(e) => setUser({...user, firstname: e.target.value})} />
         </Box>
         
-        <Box class="Binput">
-        <FormLabel  fontWeight={"bolder"} for="Username" >Username</FormLabel>
-        <Input placeholder='Username'bg={"white"} name="Username"/>
+        <Box className="Binput">
+        <FormLabel fontWeight={"bolder"} htmlFor="Surname" >Surname</FormLabel>
+        <Input placeholder='Surname' bg={"white"} id="Surname" value={user.lastname} onChange={(e) => setUser({...user, lastname: e.target.value})} />
         </Box>
         
-        <Box class="Binput">
-        <FormLabel fontWeight={"bolder"} for="Date" >Date</FormLabel>
-        <Input  type="datetime-local" bg={"white"} placeholder='Date' name="Date"/>
+        <Box className="Binput">
+        <FormLabel fontWeight={"bolder"} htmlFor="Date" >Date</FormLabel>
+        <Input  type="date" bg={"white"} placeholder='Date' id="Date" value={user.date_of_birth} onChange={(e) => setUser({...user, date_of_birth: e.target.value})}/>
         </Box>
-        
+
+        <Box className="Binput">
+        <FormLabel fontWeight={"bolder"} htmlFor="username" >Username</FormLabel>
+        <Input placeholder='Surname' bg={"white"} id="username" value={user.username} isReadOnly={true} />
+        </Box>
+
+        <Box className="Binput">
+        <FormLabel fontWeight={"bolder"} htmlFor="email" >E-mail</FormLabel>
+        <Input placeholder='Surname' bg={"white"} id="email" value={user.email} isReadOnly={true} />
+        </Box>
+
+        <Box className='box-footer' h={"20%"} mb="3em" >
+            <Button  bg={"white"} variant='outline' width={"8em"} mr="1em" color="#09264A" onClick={onOpen}>Cancel</Button>
+            <Button type="Submit" className="saveB" bg={"#09264A"} color={"white"}  width={"8em"} onClick={onSave}>Save</Button>
+        </Box>
+
         </SimpleGrid>
       </FormControl>
-        <Box className='box-footer' h={"20%"} mb="3em" >
-            <Button  bg={"white"} variant='outline' width={"8em"} mr="1em" color="#09264A">Cancel</Button>
-            <Button type="Submit" className="saveB" bg={"#09264A"} color={"white"}  width={"8em"}>Save</Button>
-        </Box>
-      </form>
+        
+     
     </Box>
+
+    <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+        <ModalHeader>Reset Changes</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+            <p>Are you sure you want to reset your changes?</p>
+        </ModalBody>
+
+        <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+            Close
+            </Button>
+            <Button colorScheme="red" onClick={()=> {setUser(originalUser); onClose();}}>Reset</Button>
+        </ModalFooter>
+        </ModalContent>
+    </Modal>
+
     
    </Flex>  
   )
