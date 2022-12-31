48461fb76a226f7d1ad72a7b813a4eea901d9a78
Kamal <elesgerlikamal04@gmail.com>
2022-12-31 15:04:56 +0400

    changed path to settings

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <Routes>
           <Route exact path="/auth" element={<Auth />}></Route>
           <Route exact path="/about" element={<Cards />}></Route>
-          <Route exact path="/me/profile" element={<ProfileUpdate />}></Route>
+          <Route exact path="/settings" element={<ProfileUpdate />}></Route>
         </Routes>
       </div>
     </Router>
