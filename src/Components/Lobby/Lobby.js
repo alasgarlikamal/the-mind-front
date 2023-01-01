dd55e08faf8c7ae1ab8c3a1cbb7f3d34999c7372
Kamal <elesgerlikamal04@gmail.com>
2023-01-01 16:38:47 +0400

    added gettinguser info

--- src/Components/Lobby/Lobby.js
+++ src/Components/Lobby/Lobby.js
@@ -1,23 +1,42 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./LobbyStyles.css";
 import CardFlip from "react-card-flip";
 import { Button,Avatar,Text,Input,Flex } from "@chakra-ui/react";
 import { IoIosArrowBack } from "react-icons/io";
+import { getUserInfo } from '../../api/getUserInfo';
 
 const CreateJoin = () => {
   const [isFlipped, setIsFlipped] = React.useState(false);
+  const [user, setUser] = useState({firstname: '', lastname: '', date_of_birth: '', username: '', email: '', avatar: {imageUrl: ''}});
+  const [errorMessage, setErrorMessage] = useState(null);
 
   const handleFlip = () => {
     setIsFlipped(!isFlipped);
   };
 
+  useEffect(() => {
+    async function getUser() {
+      const data = await getUserInfo();
+      if (!data) {
+          setErrorMessage("Something went wrong");
+          return; 
+      }
+      setUser(data);
+    }
+    getUser(); 
+  }, []);
+
+  if (errorMessage) {
+    return <div>{errorMessage}</div>;
+  }
+
   return (
     <div className="createjoin">
       <CardFlip isFlipped={isFlipped}>
         <div className="lobbycard-front">
-          <Avatar w={"5em"} h={"5em"} src="https://bit.ly/dan-abramov" mb={"1em"}/>
+          <Avatar w={"5em"} h={"5em"} src={process.env.REACT_APP_API_URL + user.avatar.imageUrl} mb={"1em"}/>
           <div className="front-text"  >
-            <Text color="white" mb={"2em"}>nickname</Text>
+            <Text color="white" mb={"2em"}>{user.username}</Text>
             </div>
           <Button bg={"#1F5378"}  w={"55%"} className="joinroombutton" onClick={handleFlip}>
             Join Room
