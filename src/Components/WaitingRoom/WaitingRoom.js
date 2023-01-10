4f269e21c741f5ef8e18b25ee3864b78da9646b6
Kamal <elesgerlikamal04@gmail.com>
2023-01-10 13:58:02 +0400

    editted waiting room

--- src/Components/WaitingRoom/WaitingRoom.js
+++ src/Components/WaitingRoom/WaitingRoom.js
@@ -1,38 +1,12 @@
 import React from "react";
 import "./WaitingRoomStyles.css";
-import { CopyIcon } from "@chakra-ui/icons";
-import { Heading,Button,Avatar,Text,Flex} from "@chakra-ui/react";
-import { AiOutlineCloseCircle } from "react-icons/ai";
-import { CopyToClipboard } from "react-copy-to-clipboard";
-
+import { Heading,Button,Avatar,Text,Flex,useClipboard, IconButton, VStack, Box} from "@chakra-ui/react";
+import { CloseIcon } from "@chakra-ui/icons";
+import PlayerRow from "./playerRow";
 
 const CreateJoin = () => {
-  const [isHovered, setIsHovered] = React.useState(false);
-  const [text, setText] = React.useState("Copy Link");
-  const [isActive, setIsActive] = React.useState(false);
-
-  const handleClick = () => {
-    setIsActive(!isActive);
-    setText("Copied!");
-    setTimeout(() => setText('Copy Link'), 1000);
-    setTimeout(() => setIsActive(false), 1000);
-    setIsHovered(!isHovered);
-  };
-
-
-  const handleMouseEnter = () => {
-    setTimeout(() => {
-      setIsHovered(true);
-    }, 20);
-  };
-
-  const handleMouseLeave = () => {
-    setTimeout(() => {
-      setIsHovered(false);
-  }, 20);
-
-  };
-
+ 
+  const { onCopy, value, setValue, hasCopied } = useClipboard("");
 
   return (
   <Flex className="PlayerList-container">
@@ -40,79 +14,43 @@ const CreateJoin = () => {
       <Flex justifyContent={"center"}>
         <Heading size={"lg"} color={"white"} textAlign={"center"}>PLAYERS 4/4</Heading>
       </Flex>
+
+      <VStack
+        spacing={4}
+        align='stretch'
+        h='160px'
+      >
+      <Box>
+        auye
+      </Box>  
+      </VStack>
+
       
       <Flex  color={"white"} mt={"4em"} flexDirection={"column"} >
           
           <Flex  p={"0.3em"} bg={"white"}  color={"black"} borderRadius={"3px"} mb={"1em"} >
 
             <Flex alignItems={"center"} justifyContent={"center"}  width={"20%"}>
-              <Avatar border={"3px solid green"} w={"2.3em"} h={"2.3em"} src="https://bit.ly/dan-abramov" />
+              <Avatar  w={"2.3em"} h={"2.3em"} src="https://bit.ly/dan-abramov" />
             </Flex>
 
             <Flex alignItems={"center"} width={"60%"}>
               <Text fontWeight={"bold"} m={"none"}  > nickname </Text>
             </Flex>
 
-            <Flex justifyContent={"center"} alignItems={"center"} width={"20%"}>
-              <AiOutlineCloseCircle fontSize={"2em"}   cursor={"pointer"}  />
-            </Flex>
-
           </Flex >
 
-          <Flex  p={"0.3em"} bg={"white"}  color={"black"} borderRadius={"3px"} mb={"1em"} >
-            
-            <Flex alignItems={"center"} justifyContent={"center"}  width={"20%"}>
-              <Avatar w={"2em"} h={"2em"} src="https://bit.ly/dan-abramov" />
-            </Flex>
-
-            <Flex alignItems={"center"} width={"60%"}>
-              <Text m={"none"} fontWeight={"bold"} > nickname </Text>
-            </Flex>
-
-            <Flex justifyContent={"center"} alignItems={"center"} width={"20%"}>
-              <AiOutlineCloseCircle fontSize={"2em"} cursor={"pointer"}  />
-            </Flex>
-
-          </Flex >
+          <PlayerRow/>
       
-          <Flex  p={"0.3em"} bg={"white"}  color={"black"} borderRadius={"3px"} mb={"1em"} >
-            
-            <Flex alignItems={"center"} justifyContent={"center"}  width={"20%"}>
-              <Avatar w={"2em"} h={"2em"} src="https://bit.ly/dan-abramov" />
-            </Flex>
-
-            <Flex alignItems={"center"} width={"60%"}>
-              <Text m={"none"} fontWeight={"bold"} > nickname </Text>
-            </Flex>
-
-            <Flex justifyContent={"center"} alignItems={"center"} width={"20%"}>
-              <AiOutlineCloseCircle fontSize={"2em"} cursor={"pointer"}  />
-            </Flex>
-
-          </Flex >
+          <PlayerRow/>
         
-          <Flex  p={"0.3em"} bg={"white"}  color={"black"} borderRadius={"3px"} mb={"1em"} >
-            
-            <Flex alignItems={"center"} justifyContent={"center"}  width={"20%"}>
-              <Avatar w={"2em"} h={"2em"} src="https://bit.ly/dan-abramov" />
-            </Flex>
+          <PlayerRow/>
 
-            <Flex alignItems={"center"} width={"60%"}>
-              <Text m={"none"} fontWeight={"bold"} > nickname </Text>
-            </Flex>
-
-            <Flex justifyContent={"center"} alignItems={"center"} width={"20%"}>
-              <AiOutlineCloseCircle fontSize={"2em"} cursor={"pointer"}  />
-            </Flex>
-
-          </Flex >
 
       </Flex>
       <Flex mt={"3em"} width={"100%"} justifyContent={"space-between"} >
-        <CopyToClipboard text="nenonushdukamal">
-            <Button  className={isActive ? 'active-copy-button' : 'copy-button'} position="sticky" isDisabled={isActive}  onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} onClick={handleClick} w={"45%"} h={"3.2em"} > {isHovered && isActive === false ? <CopyIcon transition="all ease-out 0.3s"/> : text }</Button>
-            </CopyToClipboard>
-            <Button w={"45%"} h={"3.2em"} >Start</Button>
+            <Button w={"45%"} h={"3.2em"} onClick={onCopy}>{hasCopied ? "Copied!" : "Copy"}</Button>
+            <Button w={"45%"} h={"3.2em"} colorScheme='green' >Start</Button>
       </Flex >
     </div>
   </Flex>
