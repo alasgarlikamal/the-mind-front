cceadac3cbdfc09e30eeb3800fdcf31b0eb05a39
Kamal <elesgerlikamal04@gmail.com>
2023-01-15 17:24:11 +0400

    added socket context

--- src/App.js
+++ src/App.js
@@ -6,19 +6,23 @@ import Lobby from './Components/Lobby/Lobby'
 import Error from './Components/ErrorPage/Error'
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { SocketContext, socket } from "./context/SocketContext";
 
 function App() {
   return (
+
     <Router>
       <div className="App">
-        <Routes>
-          <Route exact path="/auth" element={<Auth />}></Route>
-          <Route exact path="/about" element={<Cards />}></Route>
-          <Route exact path="/settings" element={<ProfileUpdate />}></Route>
-          <Route exact path="/lobby" element={<Lobby />}></Route>
-          <Route exact path="/waiting" element={<WaitingRoom />}></Route>
-          <Route exact path="/404" element={<Error />}></Route>
-        </Routes>
+        <SocketContext.Provider value={socket}>
+          <Routes>
+            <Route exact path="/auth" element={<Auth />}></Route>
+            <Route exact path="/about" element={<Cards />}></Route>
+            <Route exact path="/settings" element={<ProfileUpdate />}></Route>
+            <Route exact path="/lobby" element={<Lobby />}></Route>
+            <Route exact path="/waiting" element={<WaitingRoom />}></Route>
+            <Route exact path="/404" element={<Error />}></Route>
+          </Routes>
+        </SocketContext.Provider>
       </div>
     </Router>
   );
