2d7978bbc13b8e44b614ded7ca4922001b0773e4
Kamal <elesgerlikamal04@gmail.com>
2023-01-18 18:17:02 +0400

    connected to backend

--- src/Components/Game/Deck.js
+++ src/Components/Game/Deck.js
@@ -2,13 +2,12 @@ import React from "react";
 import { Circle } from "@chakra-ui/react";
 import Card from "./Card";
 
-const Deck = ({ cards }) => {
+const Deck = ({ number }) => {
 
     return (
         
         <Circle size='40vh' bg={'linear-gradient(180deg, #7C7C7C -768.67%, rgba(255, 255, 255, 0.09) 148.19%);'}>
-            
-            <Card number={99} isHoverable={false}/>
+            {number !== 0 && <Card number={number} isHoverable={false}/>}
         </Circle>
     )
 
