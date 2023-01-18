114e20cdf7a624ad1efbbf29dacb3c4485435960
Kamal <elesgerlikamal04@gmail.com>
2023-01-18 18:16:40 +0400

    connected to backend

--- src/Components/Game/PlayerPopover.js
+++ src/Components/Game/PlayerPopover.js
@@ -1,24 +1,22 @@
 import React from "react";
-import { Avatar, Button, Flex, Popover, PopoverArrow, PopoverBody, PopoverCloseButton, PopoverContent, PopoverHeader, PopoverTrigger, Portal, Text } from '@chakra-ui/react'
+import { Avatar, Button, Flex, Popover, PopoverBody, PopoverCloseButton, PopoverContent, PopoverHeader, PopoverTrigger, Portal, Text } from '@chakra-ui/react'
 import { NotAllowedIcon } from '@chakra-ui/icons'
-const PlayerPopover = () => {
-
+const PlayerPopover = ({player}) => {
 
     return (
         <>
         <Popover>
             <PopoverTrigger>
-                <Button color={'white'} _hover={{ bgGradient: 'linear(to-l, #0D366A, #01162F)' }} bg={'linear-gradient(90deg, rgba(55, 59, 68, 0.82) -43.81%, rgba(42, 109, 218, 0.82) 119.34%)'}>username</Button>
+                <Button color={'white'} _hover={{ bgGradient: 'linear(to-l, #0D366A, #01162F)' }} bg={'linear-gradient(90deg, rgba(55, 59, 68, 0.82) -43.81%, rgba(42, 109, 218, 0.82) 119.34%)'}>{player?.username}</Button>
             </PopoverTrigger>
             <Portal>
                 <PopoverContent w={'20vh'}>
-                <PopoverArrow />
                 <PopoverHeader textAlign={'center'} fontWeight={'bold'} color={'#02162F'} >PROFILE</PopoverHeader>
                 <PopoverCloseButton />
                 <PopoverBody>
                     <Flex flexDirection={'column'} gap={1} justifyContent={'center'} alignItems={'center'} >
-                        <Avatar size={'xl'} src={'/images/avatars/walter.png'}></Avatar>
-                        <Text textAlign={'center'} fontWeight={"bold"} color={'#02162F'}>username</Text>
+                        <Avatar size={'xl'} src={player?.image}></Avatar>
+                        <Text textAlign={'center'} fontWeight={"bold"} color={'#02162F'}>{player?.username}</Text>
                         <Button bg="#02162F" _hover={{bg:"#02162F"}} color={'white'} leftIcon={<NotAllowedIcon/>}>Votekick</Button>
                     </Flex>
                 </PopoverBody>
@@ -26,7 +24,7 @@ const PlayerPopover = () => {
             </Portal>
         </Popover>
 
-        <Avatar size={'xl'} src={'/images/avatars/walter.png'}></Avatar>
+        <Avatar size={'xl'} src={player?.image}></Avatar>
         </>
     )
 }
