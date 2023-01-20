40310434298f27c40e413eec685a684257ead543
Kamal <elesgerlikamal04@gmail.com>
2023-01-20 22:58:58 +0400

    connected to backend

--- src/Components/Game/ThrowingStar.js
+++ src/Components/Game/ThrowingStar.js
@@ -1,10 +1,19 @@
 import { Flex, Text, Tooltip } from "@chakra-ui/react"
+import { useContext } from "react";
+import { SocketContext } from "../../context/SocketContext";
 import './font.css';
 
 const ThrowingStar = ({star}) => {
 
+    const socket = useContext(SocketContext);
+
+    const startThrowingStarVote = () => {
+        socket.emit('throwingStarVoteStart');
+    }
+
+
     return (
-        <Flex fontSize={'8xl'} color={'white'} bgImage={'/images/throwingStar.png'} bgSize={'cover'} w={'15vh'} h={'22.5vh'} justifyContent={'center'} alignItems={'center'}>
+        <Flex fontSize={'8xl'} color={'white'} onClick={startThrowingStarVote} bgImage={'/images/throwingStar.png'} bgSize={'cover'} w={'15vh'} h={'22.5vh'} justifyContent={'center'} alignItems={'center'}>
             <Tooltip label={`Your team has ${star} throwing stars`}>
                 <Text ml={'1vh'} textShadow={'2px 2px 2px black'} textAlign={'center'} fontFamily={'Sleman'}>{star}</Text>
             </Tooltip>
