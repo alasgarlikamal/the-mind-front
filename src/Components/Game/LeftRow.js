148e388ea7cfec0fd78c0f0439859d4f272cc3fc
Kamal <elesgerlikamal04@gmail.com>
2023-01-20 22:58:47 +0400

    moved picture up

--- src/Components/Game/LeftRow.js
+++ src/Components/Game/LeftRow.js
@@ -6,19 +6,19 @@ import PlayerPopover from './PlayerPopover'
 
 const LeftRow = ({player}) => {
     return (
-        <Flex justifyContent={'center'} alignItems={'center'} flexDirection={'column'} >
-
-            <Flex justifyContent={'center'} mt={'35vh'} mb={'10vh'} flexDirection={'column'} >
-                {[...Array(player?.cardCount)].map((e, i) =>{ 
-                return(
-                <Box key={i} w={'auto'} v={'auto'} mt={'-15vw'} rotate={'90'}>
-                    <Card isHoverable={false} isSideRow={true}/>
-                </Box>)
-                })}
-            </Flex>
+        <Flex justifyContent={'center'} alignItems={'center'} flexDirection={'column'} gap={6} >
 
             <Flex size={'auto'} gap={2} flexDirection={'column'} justifyContent={'center'} alignItems={'center'}>
                 <PlayerPopover player={player}/>
+            </Flex>
+
+            <Flex justifyContent={'center'} mb={'10vh'} flexDirection={'column'} >
+                {[...Array(player?.cardCount)].map((e, i) =>{ 
+                return(
+                <Box key={i} w={'auto'} v={'auto'} mb={'-15vw'} rotate={'90'}>
+                    <Card isHoverable={false} isSideRow={true}/>
+                </Box>)
+                })}
             </Flex>
 
         </Flex>
