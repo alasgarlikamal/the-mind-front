83f02ca64c8d1861f42ac938d27986efef6ca93c
Kamal <elesgerlikamal04@gmail.com>
2023-01-21 19:46:04 +0400

    added new navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,99 +1,140 @@
-import {
-    Box,
-    Flex,
-    Link,
-    Image
-  } from "@chakra-ui/react";
-import React, { useState, useEffect} from 'react'
-import profileicon from '../../images/profileicon.svg'
-import { useLocation } from "react-router";
-import DropdownProfile from "./DropdownProfile";
-import LogOut from "./LogOut";
+import { useDisclosure, Flex, Box, Link, Divider, Button, Spacer, Image, Popover, Portal, PopoverContent, PopoverBody, PopoverTrigger, Avatar } from "@chakra-ui/react"
+import React, { useEffect, useState } from 'react'
+import { useNavigate, useLocation } from 'react-router';
+
+import { getUserInfo } from '../../api/getUserInfo';
+import { Modal, ModalBody, ModalContent, ModalOverlay, Text, ModalCloseButton } from "@chakra-ui/react";
 
 
 
-const Navbar = () => {
-    const [selected, setSelected] = useState("")
-    const [open, setOpen] = useState(false);
+export const Navbar = () => {
+
+    const navigate = useNavigate();
     const location = useLocation();
-    const [isOpenModal, setOpenModal] = useState(false);
+    const [user, setUser] = useState({firstname: '', lastname: '', date_of_birth: '', username: '', email: '', avatar: {id: 0, imageUrl: ''}});
+    const { isOpen: isLogoutOpen, onOpen: onLogoutopen, onClose: onLogoutClose } = useDisclosure();
+    const [selected, setSelected] = useState("");
 
 
+    useEffect(() => {
+        async function getUserData() {
+            const userData = await getUserInfo();
+            setUser(userData);
+          }
+        getUserData();
 
+        const path = location.pathname;
+        setSelected(path);
+        
+    }, [location]);
 
-  useEffect(() => {
-    const path = location.pathname;
-    if (path === "/") {
-      setSelected("home");
-    } else if (path === "/about") {
-      setSelected("about");
-    } else if (path === "/rules") {
-      setSelected("rules");
+    const logOut = () => {
+        localStorage.removeItem('access_token');
+        window.location.reload();
     }
-  }, [location]);
-
-  console.log(isOpenModal);
-
-  return (
-    <>
-   <Flex
-      as="nav"
-      align="baseline"
-      justify="flex-start"
-      wrap="wrap"
-      padding="1.5rem"
-      marginLeft='40px'
-      marginBottom='30px'
-      bg="transparent"
-      position={'fixed'}
-      zIndex='999'
-    >
-      <Box align="center" mr={5} >
-        <Link href="/mainpage" fontWeight="600" color="#50C9E9" fontFamily='Montserrat' lineHeight='1.905rem' fontSize='25px' >
-          THE MIND
-        </Link>
-      </Box>
 
 
-      <Flex 
-        align="center"
-        justify="space-around"
-        textAlign="center"  
-        w={"35em"}
-        ml={"8%"}
+    return (
+        <Flex w='100w' h='auto' p='2%' alignItems={'center'}>
+             <Box align="center" mr={5} >
+                <Link onClick={() => navigate('/home')} fontWeight="600" color="#50C9E9" fontSize='25px' >
+                THE MIND
+                </Link>
+            </Box>
+            <Spacer />
+
+            <Flex w='30vw' justifyContent={'center'}>
+                <Link href='/lobby' fontWeight="400" color={selected === '/lobby' ? '#50C9E9' : 'white'}   fontSize='18px' >
+                    Play
+                </Link>
+                <Spacer />
+                <Link onClick={() => navigate('/rules')} fontWeight="400" color={selected === '/rules' ? '#50C9E9' : 'white'}  fontSize='18px' >
+                    Rules
+                </Link>
+                <Spacer />
+                <Link onClick={() => navigate('/about')} fontWeight="400" color={selected === '/about' ? '#50C9E9' : 'white'} fontSize='18px'>
+                    About
+                </Link>
+            </Flex>
+
+            <Spacer /><Spacer /><Spacer /><Spacer /><Spacer /><Spacer />
+
+
+            {!user &&
+            <Flex gap={5}>
+            <Button onClick={() => navigate('/auth')} colorScheme='cyan' w={'7vw'} variant='outline'>
+                Login
+            </Button>
+            <Button onClick={() => navigate('/auth')} colorScheme='cyan' color={'white'} w={'7vw'} variant='solid'>
+                Register
+            </Button>
+            </Flex>
+            }
+            
+            {user && 
+            <Popover>
+                <PopoverTrigger>
+                <Image _hover={{cursor: 'pointer'}} boxSize='auto' src={'/images/profile.svg'}/>
+                </PopoverTrigger>
+                <Portal>
+                    <PopoverContent w='auto'>
+                    <PopoverBody >
+                    <Flex flexDirection={'column'} gap={2}>
+
+                        <Flex gap={1}>
+                            <Avatar border={'1px solid black'} src={user.avatar.imageUrl}/>
+                            <Flex flexDirection={'column'}  >
+                                <Box w='auto'>{user.firstname} {user.lastname}</Box>
+                                <Box mt={'-1%'}>@{user.username}</Box>
+                            </Flex>
+                        </Flex>
+
+                        <Divider />
+
+                        <Flex flexDir='column' gap={2} alignItems='start'>
+
+                            <Flex gap={2} cursor='pointer' onClick={() => navigate('/me')}>
+                                <Image boxSize='auto' src={'/images/profileVector.svg'}/>
+                                Profile
+                            </Flex>
+                            <Flex gap={2} cursor='pointer' onClick={() => navigate('/settings')}>
+                                <Image boxSize='auto' src={'/images/settingsVector.svg'}/>
+                                Settings
+                            </Flex>
+                            <Flex gap={2} cursor='pointer' onClick={() => onLogoutopen()}>
+                                <Image boxSize='auto' src={'/images/logoutVector.svg'}/>
+                                Log out
+                            </Flex>
+                        </Flex>
+                    </Flex>
+                    </PopoverBody>
+                    </PopoverContent>
+                </Portal>
+            </Popover>
         
-      >
-        <Box>
-        <Link href="#" fontWeight="400" color={selected === 'play' ? '#50C9E9' : 'white'} onClick={()=>setSelected("play")}  fontSize='18px' >
-          Play
-        </Link>
-        </Box>
-        <Box>
-        <Link href="#" fontWeight="400" color={selected === 'rules' ? '#50C9E9' : 'white'} onClick={()=>setSelected("rules")} fontSize='18px' >
-          Rules
-        </Link>
-        </Box>
-        <Box>
-        <Link href="/about" fontWeight="400" color={selected === 'about' ? '#50C9E9' : 'white'} onClick={()=>setSelected("about")} fontSize='18px'>
-          About
-        </Link>
-        </Box>
+            }
+            <Modal isOpen={isLogoutOpen} onClose={onLogoutClose}>
+                <ModalOverlay />
+                <ModalContent w='auto'>
+                
+                <ModalCloseButton />
+                <ModalBody p='5vh'>
+                <Flex flexDir='column' h='20vh' w='auto' alignItems='center'>
+                <Text fontSize='xl' w='30vw' textAlign='center' fontWeight='bold' >You are attempting to log out of THE MIND</Text>
+                    <Spacer/>
+                <Text color='gray' >Are you sure?</Text>
+                <Spacer/>
+                <Spacer/>
+                    <Button onClick={()=> logOut()} bgColor='#02162F' w='20vw' _hover={{bgColor: '#02162F'}} color='white' >
+                    Log out
+                 </Button>
+                </Flex>
+                </ModalBody>
+                </ModalContent>
+            </Modal>
+            <Spacer />
 
-        
-      </Flex>
-      <Box>
-        <Image onClick={() => {setOpen(!open)}} marginLeft='84rem'  marginTop='-2.6rem' cursor='pointer' src={profileicon}/>
-        <DropdownProfile setOpenModal = {setOpenModal} open = {open} classname='dropdownProfile' />
-      </Box>
 
-        
-
-    </Flex>
-    
-    {isOpenModal && <LogOut setOpenModal = {setOpenModal} isOpenModal= {isOpenModal} />}
-
-    </>
-  )
+        </Flex>
+    )
 }
-
-export default Navbar
