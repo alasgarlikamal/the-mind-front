c24c14bc635021ecc77a5b7dd4f90749dbffd528
Kamal <elesgerlikamal04@gmail.com>
2023-01-21 19:45:41 +0400

    connected to backend

--- src/Components/Statistics/Statistics.js
+++ src/Components/Statistics/Statistics.js
@@ -1,35 +1,60 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./StatisticsStyles.css";
-import { Avatar,Text,Flex,Box,CircularProgress, CircularProgressLabel} from "@chakra-ui/react";
+import { Avatar,Text,Flex,Box,CircularProgress, CircularProgressLabel, Td, Tr, Table, TableContainer, Tbody} from "@chakra-ui/react";
+import { Navbar } from '../Navbar/Navbar';
+import { getUserInfo } from '../../api/getUserInfo';
+import { useNavigate } from 'react-router';
+
 export default function Statistics() {
+
+  const [user, setUser] = useState({firstname: '', lastname: '', date_of_birth: '', number_of_games_played: 0, max_level_reached: 0, elo: 0, username: '', email: '', avatar: {id: 0, imageUrl: ''}});
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    async function getUserData() {
+      const userData = await getUserInfo();
+      userData && setUser(userData);
+      !userData && navigate('/auth')
+    }
+    getUserData();
+  }, [navigate]);
+
+
   return (
+    <Flex flexDir='column'>
     <div className="statisticsBody">
+      <Navbar/>
       <Flex className="statisticsCard" p={"4em"} >
         
         <Flex className='card1 leftc' bg={"white"} height={"100%"} >
             <Flex height={"40%"} justifyContent={"center"} alignItems={"center"} >
                 <Box width={"40%"}>
-                <Avatar w={"5em"} h={"5em"} ></Avatar>
+                <Avatar w={"5em"} h={"5em"} src={user.avatar.imageUrl} ></Avatar>
                 </Box>
                 <Box w={"60%"}>
-                <Text fontSize={"1.3em"} ><b>@username</b></Text>
+                <Text fontSize={"1.3em"} ><b>@{user.username}</b></Text>
                 </Box>
             
             </Flex>
             <Flex height={"60%"} >
-                <Flex flexDirection={"column"} width={"40%"}>
-                    <Text>Name</Text>
-                    <Text>Surname</Text>
-                    <Text>Birthday</Text>
-                    <Text>E-mail</Text>
-                </Flex>
-                <Flex flexDirection={"column"} width={"60%"}>
-                    <Text>Murad</Text>
-                    <Text>Isayev</Text>
-                    <Text>28/09/2003</Text>
-                    <Text>muradisayev@gmail.com</Text>
-                </Flex>
-            
+            <TableContainer>
+              <Table variant='simple'>
+                <Tbody>
+                  <Tr>
+                    <Td>Name</Td>
+                    <Td>{user.firstname} {user.lastname}</Td>  
+                  </Tr>
+                  <Tr>
+                    <Td>Birthday</Td>
+                    <Td>{user.date_of_birth}</Td>
+                  </Tr>
+                  <Tr>
+                    <Td>E-mail</Td>
+                    <Td>{user.email}</Td>
+                  </Tr>
+                </Tbody>
+              </Table>
+            </TableContainer>
             </Flex>
         </Flex>
         <Flex className='rightc' flexDirection={"column"} height={"100%"}>
@@ -38,8 +63,8 @@ export default function Statistics() {
             <Text><b>Max level</b></Text>
             </Flex>
             <Flex>
-            <CircularProgress size={"2em"} value={50} color='blue.900' >
-            <CircularProgressLabel fontSize={"0.5em"}><b>6</b></CircularProgressLabel>
+            <CircularProgress size={"2em"} value={user.max_level_reached*100/12} color='blue.900' >
+            <CircularProgressLabel fontSize={"0.5em"}><b>{user.max_level_reached}</b></CircularProgressLabel>
             </CircularProgress>
             </Flex>
             
@@ -49,7 +74,7 @@ export default function Statistics() {
             <Text>Games played</Text>
             </Flex>
             <Flex>
-            <Text fontWeight={"bold"} fontSize={"24px"} >100</Text>
+            <Text fontWeight={"bold"} fontSize={"24px"} >{user.number_of_games_played}</Text>
             </Flex>
             
         </Flex>
@@ -58,12 +83,16 @@ export default function Statistics() {
             <Text>Rating</Text>
             </Flex>
             
-            <Text fontSize={"24px"} fontWeight={"bold"}>270</Text>
+            <Text fontSize={"24px"} fontWeight={"bold"}>{user.elo}</Text>
         </Flex>
         </Flex>
         
 
       </Flex>
+      <Flex justifyContent={'center'}>
+        <Text style={{ position: 'absolute', bottom: 0 }} fontSize={'xs'} marginBottom={'1vw'} textAlign={'center'} color={'#9B9B9B'}>All rights reserved © 2022 The Mind</Text>
+      </Flex>
     </div>
+    </Flex>
   )
 }
