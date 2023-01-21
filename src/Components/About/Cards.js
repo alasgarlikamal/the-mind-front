8597b88eccff958183f6fb5422fc57111285782c
Kamal <elesgerlikamal04@gmail.com>
2023-01-21 19:47:16 +0400

    added navbar

--- src/Components/About/Cards.js
+++ src/Components/About/Cards.js
@@ -2,6 +2,8 @@ import React, { useEffect} from "react";
 import FlippableCard from "./FlippableCard";
 import "./Cards.css";
 import getAbout from "../../api/getAbout";
+import { Box, Flex, Text } from "@chakra-ui/react";
+import { Navbar } from "../Navbar/Navbar";
 
 
 const Cards = () => {
@@ -15,19 +17,26 @@ const Cards = () => {
   }, []);
 
   return (
-    <div className="cards-container">
-      {about && about.map((item) => {
-        return (
-          <FlippableCard
-            key={item.id}
-            front={item.role}
-            back={item.fullName}
-            photo={item?.photo}
-            used={item.text}
-          />
-        );
-      })}
-    </div>
+    <Box w='100vw' h='100vh' bgColor='black' backgroundImage={"url('/images/image 3.png')"} bgRepeat={'no-repeat'} bgSize={'cover'}>
+      <Navbar />
+      <div className="cards-container">
+        {about && about.map((item) => {
+          return (
+            <FlippableCard
+              key={item.id}
+              front={item.role}
+              back={item.fullName}
+              photo={item?.avatar.imageUrl}
+              used={item.text}
+            />
+          );
+        })}
+      </div>
+      <Flex justifyContent={'center'}>
+        <Text style={{ position: 'absolute', bottom: 0 }} fontSize={'xs'} marginBottom={'1vw'} textAlign={'center'} color={'#9B9B9B'}>All rights reserved © 2022 The Mind</Text>
+      </Flex>
+    </Box>
+
   );
 };
 
