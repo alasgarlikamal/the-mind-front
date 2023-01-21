bfcfef06a11c6099d5ef495a4449e50a4fd6f8f7
Kamal <elesgerlikamal04@gmail.com>
2023-01-21 21:05:05 +0400

    bg pos center

--- src/Components/Home/MainPage.js
+++ src/Components/Home/MainPage.js
@@ -10,7 +10,7 @@ const MainPage = () => {
 
   
     return (
-      <Flex bgImage={'/images/main-page.jpg'} h='100vh'  flexDir='column' bgRepeat={'no-repeat'} bgSize='cover'>
+      <Flex bgImage={'/images/main-page.jpg'} h='100vh'  flexDir='column' bgPos={'center'} bgRepeat={'no-repeat'} bgSize='cover'>
         <Navbar/>
       <div className='mainpage-container'>
         <Button  width="11.688rem" height='4.25rem' color='#16163D' fontSize='2.063rem' fontFamily='Montserrat' borderRadius='0.75rem' right='36.5rem' top='10.438rem' background='rgba(255, 255, 255, 0.87)' boxShadow='6px 4px 15px rgba(11, 11, 52, 0.25);'>Play</Button>
