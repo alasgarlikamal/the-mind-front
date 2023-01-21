580151ac0b15d7b20a4fa46f2a0053983aeafaf1
Kamal <elesgerlikamal04@gmail.com>
2023-01-21 19:47:25 +0400

    added navbar

--- src/App.js
+++ src/App.js
@@ -7,19 +7,14 @@ import Error from './Components/ErrorPage/Error'
 import Statistics from './Components/Statistics/Statistics'
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import GameRoom from "./Components/GameRoom/GameRoom";
-import Navbar from "./Components/Navbar/Navbar";
 import { SocketContext, socket } from "./context/SocketContext";
 import Game from "./Components/Game/Game";
-import Navbar2 from "./Components/Navbar/Navbar2";
-import LogOut from "./Components/Navbar/LogOut";
 
 
 function App() {
   return (
 
     <Router>
-      <Navbar2/>
       <div className="App">
 
         <SocketContext.Provider value={socket}>
@@ -29,11 +24,9 @@ function App() {
           <Route exact path="/settings" element={<ProfileUpdate />}></Route>
           <Route exact path="/lobby" element={<Lobby />}></Route>
           <Route exact path="/waiting" element={<WaitingRoom />}></Route>
-          <Route exact path="/gameroom" element={<GameRoom/>}></Route>
           <Route exact path="/404" element={<Error />}></Route>
-          <Route exact path="/statistics" element={<Statistics />}></Route>
+          <Route exact path="/me" element={<Statistics />}></Route>
           <Route exact path='/game' element={<Game />}></Route>
-          <Route exact path="/logout" element={<LogOut/>}></Route>
 
         </Routes>
         </SocketContext.Provider>
